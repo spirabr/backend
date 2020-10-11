b948eb1d1f20c15bdc0fae4c462a76ea3c9ee685
thlasso <thaislasso@gmail.com>
2020-10-11 19:22:44 -0300

    fix: run lint and change to es modules
    Signed-off-by: Alexia Scheffer <ac.scheffer@usp.br>
    Signed-off-by: Matheus Santos <mattsantos.conce@gmail.com>
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>
    Signed-off-by: Thais Lasso <thlasso@usp.br>
    Signed-off-by: Vinicius Pereira <vini.gpereira2014@gmail.com>
    Signed-off-by: Wander Souza <wanderdasouza@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import mongoMiddleware from './mongoConnection.js';
 
-const { mongoMiddleware } = require('./mongoConnection')
+const app = express();
+const port = 3000;
 
 app.get('/', mongoMiddleware, async (req, res) => {
-  const response = await req.dbConnection.collection('users').findOne({ userId: 0 })
-  res.json(response)
-})
+  const response = await req.dbConnection.collection('users').findOne({ userId: 0 });
+  res.json(response);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/mock.js
+++ src/mock.js
@@ -1,4 +1,4 @@
 db.users.insertMany([
-  { userId: 0, name: 'Vinicius' }, 
-  { userId: 1, name: 'Victor' }
-])
+  { userId: 0, name: 'Vinicius' },
+  { userId: 1, name: 'Victor' },
+]);
