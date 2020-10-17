2674256ef9ed2a7c40270d91d5fddcb8a143fb51
mattconce <mattsantos.conce@gmail.com>
2020-10-17 19:34:33 -0300

    feat: Create some first routes
    Signed-off-by: Matheus Santos <mattsantos.conce@gmail.com>
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>
    Signed-off-by: Vinicius Pereira <vini.gpereira2014@gmail.com>

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,9 +5,18 @@ const router = express.Router();
 
 router.get("/", mongoMiddleware, async (req, res) => {
   const response = await req.dbConnection
-    .collection("users")
-    .findOne({ userId: 0 });
-  res.json(response);
+    .collection("records")
+    .find({}).toArray();
+  res.json({data:response});
 });
+
+router.get("/:cpf", mongoMiddleware, async (req, res) => {
+  const cpf = req.params.cpf
+  const response = await req.dbConnection
+    .collection("records")
+    .findOne({cpf});
+  res.json({data:response});
+});
+
 
 export default router;
