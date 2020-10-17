28a595ab5f4ddf6bd9ede7f29b6b35ece15a48dd
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-17 18:20:09 -0300

    style: format with prettier
    Signed-off-by: Vinicius Pereira <vini.gpereira2014@gmail.com>
    Signed-off-by: Matheus Santos <mattsantos.conce@gmail.com>
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>

--- src/db/mock.js
+++ src/db/mock.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 db.users.insertMany([
-  { userId: 0, name: 'Vinicius' },
-  { userId: 1, name: 'Victor' },
+  { userId: 0, name: "Vinicius" },
+  { userId: 1, name: "Victor" },
 ]);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import mongoMiddleware from '../db/mongoConnection.js';
+import express from "express";
+import mongoMiddleware from "../db/mongoConnection.js";
 
 const router = express.Router();
 
-router.get('/', mongoMiddleware, async (req, res) => {
-  const response = await req.dbConnection.collection('users').findOne({ userId: 0 });
+router.get("/", mongoMiddleware, async (req, res) => {
+  const response = await req.dbConnection
+    .collection("users")
+    .findOne({ userId: 0 });
   res.json(response);
 });
 
