48671bc68f9f25e3cdef94989d7fc0684a80bf91
mattconce <mattsantos.conce@gmail.com>
2020-10-17 19:34:33 -0300

    feat: Create some first routes
    Signed-off-by: Matheus Santos <mattsantos.conce@gmail.com>
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>
    Signed-off-by: Vinicius Pereira <vini.gpereira2014@gmail.com>

--- src/db/mock.js
+++ src/db/mock.js
@@ -1,5 +1,9 @@
 // eslint-disable-next-line no-undef
-db.users.insertMany([
-  { userId: 0, name: "Vinicius" },
-  { userId: 1, name: "Victor" },
+db.records.insertMany([
+  { name: "Vinicius", cpf: "909090666", email: "email@us.br", age: "10", gender: "",
+   audiourl1: "", audiourl2: "", audiourl3: "", audiourl4: "" },
+  { name: "Matheus", cpf: "909090688", email: "email@us.br", age: "10", gender: "",
+   audiourl1: "", audiourl2: "", audiourl3: "", audiourl4: "" },
+  { name: "Ricardo", cpf: "909090644", email: "email@us.br", age: "10", gender: "",
+   audiourl1: "", audiourl2: "", audiourl3: "", audiourl4: "" },
 ]);
