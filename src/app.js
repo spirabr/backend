9eb00042cd088d439d75527fa086dfac203677ab
mattconce <mattsantos.conce@gmail.com>
2020-10-28 22:03:42 -0300

    feat: Create first routes, completion of the missing routes
    Signed-off-by: Matheus Santos <mattsantos.conce@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import routes from "./routes/routes.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(routes);
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,7 +12,7 @@ router.get("/", mongoMiddleware, async (req, res) => {
 });
 
 router.get("/:cpf", mongoMiddleware, async (req, res) => {
-  const cpf = req.params.cpf
+  const cpf = req.params.cpf;
   
   const response = await req.dbConnection
     .collection("records")
@@ -21,5 +21,59 @@ router.get("/:cpf", mongoMiddleware, async (req, res) => {
   res.json({data:response});
 });
 
+router.post("/", mongoMiddleware, async (req, res) => {
+  const user = {name: req.body.name, 
+    cpf: req.body.cpf, 
+    email: req.body.email,
+    age: req.body.age, 
+    gender: req.body.gender, 
+    audiourl1: "",
+    audiourl2: "", 
+    audiourl3: "", 
+    audiourl4: ""};
+
+  try {
+  const response = await req.dbConnection
+    .collection("records")
+    .insertOne(user);
+
+    res.status(201).json({Response: response});
+  } catch (err) {
+    res.status(404).json(err.message);
+  }
+  
+});
+
+router.patch("/:cpf", mongoMiddleware, async (req, res) => {
+  const cpf = req.params.cpf;
+  const name = req.body.name
+  const email = req.body.email;
+  const age = req.body.age;
+  const gender = req.body.gender;
+
+  try {
+    const response = await req.dbConnection
+      .collection("records")
+      .updateOne({"cpf":cpf}, 
+        {$set: {"name": name, "email": email, "age": age, "gender": gender}}, {upsert: true});
+      res.json({Response: response});
+
+  } catch (err) {
+    res.status(404).json(err.message);
+  }
+});
+
+router.delete("/:cpf", mongoMiddleware, async (req, res) => {
+  const cpf = req.params.cpf;
+
+  try {
+    const response = await req.dbConnection
+      .collection("records")
+      .deleteOne({"cpf": cpf});
+    res.json({Response: response});
+  } catch (err) {
+    res.status(404).json(err.message);
+  }
+});
 
 export default router;
