9fd803dfcd2ab76fd5319a145af933c26995a87a
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-31 18:46:23 -0300

    chore: add validator to post route
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>
    Signed-off-by: Thais Lasso <thlasso@usp.br>
    Signed-off-by: Vinicius Pereira <vini.gpereira2014@gmail.com>

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,59 +5,66 @@ const router = express.Router();
 
 router.get("/", mongoMiddleware, async (req, res) => {
   const response = await req.dbConnection
-    .collection("records")
-    .find({}).toArray();
+    .collection("samples")
+    .find({})
+    .toArray();
 
-  res.json({data:response});
+  res.json({ data: response });
 });
 
 router.get("/:cpf", mongoMiddleware, async (req, res) => {
   const cpf = req.params.cpf;
-  
+
   const response = await req.dbConnection
-    .collection("records")
-    .findOne({cpf});
-    
-  res.json({data:response});
+    .collection("samples")
+    .findOne({ cpf });
+
+  res.json({ data: response });
 });
 
 router.post("/", mongoMiddleware, async (req, res) => {
-  const user = {name: req.body.name, 
-    cpf: req.body.cpf, 
-    email: req.body.email,
-    age: req.body.age, 
-    gender: req.body.gender, 
-    audiourl1: "",
-    audiourl2: "", 
-    audiourl3: "", 
-    audiourl4: ""};
+  const { patientId, collector } = req.body;
+
+  if (!patientId || !collectorIsValid(collector)) {
+    res.status(400).send("Patient id or collector data is invalid.");
+  }
+
+  const sample = {
+    patientId: patientId,
+    collector: collector,
+    audioUrl1: "",
+    audioUrl2: "",
+    audioUrl3: "",
+    audioUrl4: "",
+  };
 
   try {
-  const response = await req.dbConnection
-    .collection("records")
-    .insertOne(user);
+    const response = await req.dbConnection
+      .collection("samples")
+      .insertOne(sample);
 
-    res.status(201).json({Response: response});
+    res.status(201).json({ response: response });
   } catch (err) {
-    res.status(404).json(err.message);
+    res.status(500).json(err.message);
   }
-  
 });
 
 router.patch("/:cpf", mongoMiddleware, async (req, res) => {
   const cpf = req.params.cpf;
-  const name = req.body.name
+  const name = req.body.name;
   const email = req.body.email;
   const age = req.body.age;
   const gender = req.body.gender;
 
   try {
     const response = await req.dbConnection
-      .collection("records")
-      .updateOne({"cpf":cpf}, 
-        {$set: {"name": name, "email": email, "age": age, "gender": gender}}, {upsert: true});
-      res.json({Response: response});
-
+      .collection("samples")
+      .updateOne(
+        { cpf: cpf },
+        { $set: { name: name, email: email, age: age, gender: gender } },
+        { upsert: true }
+      );
+    res.json({ Response: response });
   } catch (err) {
     res.status(404).json(err.message);
   }
@@ -68,12 +75,16 @@ router.delete("/:cpf", mongoMiddleware, async (req, res) => {
 
   try {
     const response = await req.dbConnection
-      .collection("records")
-      .deleteOne({"cpf": cpf});
-    res.json({Response: response});
+      .collection("samples")
+      .deleteOne({ cpf: cpf });
+    res.json({ Response: response });
   } catch (err) {
     res.status(404).json(err.message);
   }
 });
+
+function collectorIsValid(collector) {
+  return collector && collector.name && collector.hospital;
+}
 
 export default router;
