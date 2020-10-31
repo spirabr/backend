c2f127c9eb2fc1ef59233e0613bf6762aeac142c
viniciusgpereira <vini.gpereira2014@gmail.com>
2020-10-31 18:45:22 -0300

    refactor: update data structure
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>
    Signed-off-by: Thais Lasso <thlasso@usp.br>
    Signed-off-by: Vinicius Pereira <vini.gpereira2014@gmail.com>

--- src/db/mock.js
+++ src/db/mock.js
@@ -1,9 +1,36 @@
 // eslint-disable-next-line no-undef
-db.records.insertMany([
-  { name: "Vinicius", cpf: "909090666", email: "email@us.br", age: "10", gender: "",
-   audiourl1: "", audiourl2: "", audiourl3: "", audiourl4: "" },
-  { name: "Matheus", cpf: "909090688", email: "email@us.br", age: "10", gender: "",
-   audiourl1: "", audiourl2: "", audiourl3: "", audiourl4: "" },
-  { name: "Ricardo", cpf: "909090644", email: "email@us.br", age: "10", gender: "",
-   audiourl1: "", audiourl2: "", audiourl3: "", audiourl4: "" },
+db.samples.insertMany([
+  {
+    patientId: "9124192448",
+    collector: {
+      name: "Vinicius",
+      hospital: "Albert Einstein",
+    },
+    audioUrl1: "",
+    audioUrl2: "",
+    audioUrl3: "",
+    audioUrl4: "",
+  },
+  {
+    patientId: "9124191240",
+    collector: {
+      name: "Ricardo",
+      hospital: "Albert Einstein",
+    },
+    audioUrl1: "",
+    audioUrl2: "",
+    audioUrl3: "",
+    audioUrl4: "",
+  },
+  {
+    patientId: "9124112440",
+    collector: {
+      name: "Thais",
+      hospital: "Albert Einstein",
+    },
+    audioUrl1: "",
+    audioUrl2: "",
+    audioUrl3: "",
+    audioUrl4: "",
+  },
 ]);
