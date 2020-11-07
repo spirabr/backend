90bc4f6c54cfe574cf99606853a55dad8ed88f0a
victorhmp <victor2142@gmail.com>
2020-11-07 19:24:30 -0300

    chore: refactor logic out of router and into controllers

--- src/db/mongoConnection.js
+++ src/db/mongoConnection.js
@@ -1,15 +1,27 @@
 import MongoClient from "mongodb";
 
-const setupDb = async () => {
-  const connection = await MongoClient.connect(process.env.MONGODB_URL);
-  return connection.db(process.env.MONGODB_DATABASE_NAME);
+export const setupDb = async () => {
+  try {
+    const connection = await MongoClient.connect(process.env.MONGODB_URL);
+    const isTest = process.env.NODE_ENV === "test";
+
+    const dbName = `${process.env.MONGODB_DATABASE_NAME}${
+      isTest ? "_test" : ""
+    }`;
+
+    return connection.db(dbName);
+  } catch (error) {
+    console.error(error);
+
+    throw new Error("Failed to connect to MongoDB");
+  }
 };
 
 export default async (req, res, next) => {
   const dbConnection = await setupDb();
 
   if (!dbConnection) {
-    res.status(500).send({ error: "Failed to connect to mongodb!" });
+    res.status(500).send({ error: "Failed to connect to MongoDB" });
   }
 
   req.dbConnection = dbConnection;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,122 +1,23 @@
 import express from "express";
+import {
+  getAll,
+  create,
+  getSampleByPatientId,
+  updateSample,
+  deleteSampleByPatientId,
+} from "../controllers/samples.js";
 import mongoMiddleware from "../db/mongoConnection.js";
-
-function collectorIsValid(collector) {
-  return collector && collector.name && collector.hospital;
-}
-
-function getSampleUpdateObj(updatesFromBody) {
-  return Object.entries(updatesFromBody).reduce((acc, [key, value]) => {
-    if (value) acc[key] = value;
-
-    return acc;
-  }, {});
-}
 
 const router = express.Router();
 
-router.get("/", mongoMiddleware, async (req, res) => {
-  const response = await req.dbConnection
-    .collection("samples")
-    .find({})
-    .toArray();
+router.get("/", mongoMiddleware, getAll);
 
-  res.status(200).json({ data: response });
-});
+router.post("/", mongoMiddleware, create);
 
-router.get("/:patientId", mongoMiddleware, async (req, res) => {
-  const { patientId } = req.params;
+router.get("/:patientId", mongoMiddleware, getSampleByPatientId);
 
-  const response = await req.dbConnection
-    .collection("samples")
-    .findOne({ patientId });
+router.patch("/:patientId", mongoMiddleware, updateSample);
 
-  res.status(200).json({ data: response });
-});
-
-router.post("/", mongoMiddleware, async (req, res) => {
-  const { patientId, collector } = req.body;
-
-  if (!patientId || !collectorIsValid(collector)) {
-    res.status(400).send("Patient id or collector data is invalid.");
-    return;
-  }
-
-  const sample = {
-    patientId,
-    collector,
-    audioUrl1: "",
-    audioUrl2: "",
-    audioUrl3: "",
-    audioUrl4: "",
-  };
-
-  try {
-    const response = await req.dbConnection
-      .collection("samples")
-      .insertOne(sample);
-
-    res.status(201).json({ response });
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
-
-router.patch("/:patientId", mongoMiddleware, async (req, res) => {
-  const { patientId } = req.params;
-  const { collector, audioUrl1, audioUrl2, audioUrl3, audioUrl4 } = req.body;
-
-  const sample = await req.dbConnection
-    .collection("samples")
-    .findOne({ patientId });
-
-  if (!sample) {
-    res.status(404).send(`Patient ${patientId} does not exist on database.`);
-    return;
-  }
-
-  const updatesFromBody = {
-    collector: {
-      name:
-        collector && collector.name ? collector.name : sample.collector.name,
-      hospital:
-        collector && collector.hospital
-          ? collector.hospital
-          : sample.collector.hospital,
-    },
-    audioUrl1,
-    audioUrl2,
-    audioUrl3,
-    audioUrl4,
-  };
-
-  try {
-    const response = await req.dbConnection
-      .collection("samples")
-      .updateOne(
-        { patientId },
-        { $set: getSampleUpdateObj(updatesFromBody) },
-        { upsert: true }
-      );
-
-    res.status(200).json({ response });
-  } catch (err) {
-    res.status(404).json(err.message);
-  }
-});
-
-router.delete("/:patientId", mongoMiddleware, async (req, res) => {
-  const { patientId } = req.params;
-
-  try {
-    const response = await req.dbConnection
-      .collection("samples")
-      .deleteOne({ patientId });
-
-    res.status(200).json({ response });
-  } catch (err) {
-    res.status(404).json(err.message);
-  }
-});
+router.delete("/:patientId", mongoMiddleware, deleteSampleByPatientId);
 
 export default router;
