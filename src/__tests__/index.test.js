83361ad5ba1652dac6e674e7bbeed3573376646c
victorhmp <victor2142@gmail.com>
2020-11-07 19:24:53 -0300

    tests:initial test plan

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,35 @@
+import request from "supertest";
+
+import { app } from "../app";
+import { setupDb } from "../db/mongoConnection";
+
+describe("'/' route handlers", () => {
+  let db;
+
+  beforeAll(async () => {
+    db = await setupDb();
+
+    return db;
+  });
+
+  beforeEach(async () => {
+    await db.dropDatabase(`${process.env.DB_NAME}_test`);
+  });
+
+  afterAll(async () => db.close());
+
+  it("GET should return all samples", async () => {
+    const { body } = await request(app).get("/");
+
+    expect(body.data).toBeDefined();
+  });
+  it.todo("POST should return created sample");
+
+  it.todo("POST should return 401 if input is invalid");
+});
+
+describe("'/:patientId' route handlers", () => {
+  it.todo("GET should return valid sample");
+  it.todo("PATCH should return updated sample");
+  it.todo("DELETE should return deleted sample");
+});
