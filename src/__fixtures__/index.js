fc12447d94c18c3767967d77152065cd6038f468
victorhmp <victor2142@gmail.com>
2020-11-17 16:01:22 -0300

    chore:add new unit tests

---
+++ src/__fixtures__/index.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line import/prefer-default-export
+export const mockedSamples = [
+  {
+    patientId: "9124192448",
+    collector: {
+      name: "Vinicius",
+      hospital: "Albert Einstein",
+    },
+    audioUrl1: "",
+    audioUrl2: "",
+    audioUrl3: "",
+    audioUrl4: "",
+    timeStamp: new Date("2020-05-10"),
+  },
+  {
+    patientId: "9124191240",
+    collector: {
+      name: "Ricardo",
+      hospital: "Albert Einstein",
+    },
+    audioUrl1: "",
+    audioUrl2: "",
+    audioUrl3: "",
+    audioUrl4: "",
+    timeStamp: new Date("2020-04-20"),
+  },
+  {
+    patientId: "9124112440",
+    collector: {
+      name: "Thais",
+      hospital: "Albert Einstein",
+    },
+    audioUrl1: "",
+    audioUrl2: "",
+    audioUrl3: "",
+    audioUrl4: "",
+    timeStamp: new Date("2020-05-30"),
+  },
+];
--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -2,6 +2,7 @@ import request from "supertest";
 
 import { app } from "../app";
 import { setupDb } from "../db/mongoConnection";
+import { mockedSamples } from "../__fixtures__";
 
 describe("'/' route handlers", () => {
   let db;
@@ -19,49 +20,18 @@ describe("'/' route handlers", () => {
 
     await db.createCollection("samples");
 
-    await db.collection("samples").insertMany([
-      {
-        patientId: "9124192448",
-        collector: {
-          name: "Vinicius",
-          hospital: "Albert Einstein",
-        },
-        audioUrl1: "",
-        audioUrl2: "",
-        audioUrl3: "",
-        audioUrl4: "",
-      },
-      {
-        patientId: "9124191240",
-        collector: {
-          name: "Ricardo",
-          hospital: "Albert Einstein",
-        },
-        audioUrl1: "",
-        audioUrl2: "",
-        audioUrl3: "",
-        audioUrl4: "",
-      },
-      {
-        patientId: "9124112440",
-        collector: {
-          name: "Thais",
-          hospital: "Albert Einstein",
-        },
-        audioUrl1: "",
-        audioUrl2: "",
-        audioUrl3: "",
-        audioUrl4: "",
-      },
-    ]);
+    await db.collection("samples").insertMany(mockedSamples);
   });
 
-  afterAll(async () => db.close());
+  afterAll(async () => {
+    await db.collection("samples").drop();
+    db.close();
+  });
 
   it("GET should return all samples", async () => {
     const { body } = await request(app).get("/");
 
-    const mockedSamples = [
+    expect(body.data).toEqual([
       {
         patientId: "9124192448",
         collector: {
@@ -72,6 +42,7 @@ describe("'/' route handlers", () => {
         audioUrl2: "",
         audioUrl3: "",
         audioUrl4: "",
+        timeStamp: "2020-05-10T00:00:00.000Z",
       },
       {
         patientId: "9124191240",
@@ -83,6 +54,7 @@ describe("'/' route handlers", () => {
         audioUrl2: "",
         audioUrl3: "",
         audioUrl4: "",
+        timeStamp: "2020-04-20T00:00:00.000Z",
       },
       {
         patientId: "9124112440",
@@ -94,10 +66,9 @@ describe("'/' route handlers", () => {
         audioUrl2: "",
         audioUrl3: "",
         audioUrl4: "",
+        timeStamp: "2020-05-30T00:00:00.000Z",
       },
-    ];
-
-    expect(body.data).toEqual(mockedSamples);
+    ]);
   });
 
   it("POST should return created sample", async () => {
@@ -120,11 +91,61 @@ describe("'/' route handlers", () => {
     });
   });
 
-  it.todo("POST should return 401 if input is invalid");
+  it("POST should return status code 400 if input is invalid", async () => {
+    const { status } = await request(app)
+      .post("/")
+      .send({
+        patientId: "",
+        collector: { hospital: "Test" },
+      });
+
+    expect(status).toBe(400);
+  });
 });
 
 describe("'/:patientId' route handlers", () => {
-  it.todo("GET should return valid sample");
+  let db;
+
+  beforeAll(async () => {
+    db = await setupDb();
+
+    await db.createCollection("samples");
+
+    return db;
+  });
+
+  beforeEach(async () => {
+    await db.collection("samples").drop();
+
+    await db.createCollection("samples");
+
+    await db.collection("samples").insertMany(mockedSamples);
+  });
+
+  afterAll(async () => {
+    await db.collection("samples").drop();
+    db.close();
+  });
+
+  it("GET should return valid sample", async () => {
+    const { body, status } = await request(app).get("/9124192448");
+
+    expect(status).toBe(200);
+    expect(body.response).toEqual({
+      patientId: "9124192448",
+      collector: {
+        name: "Vinicius",
+        hospital: "Albert Einstein",
+      },
+      audioUrl1: "",
+      audioUrl2: "",
+      audioUrl3: "",
+      audioUrl4: "",
+      timeStamp: "2020-05-10T00:00:00.000Z",
+    });
+  });
+
   it.todo("PATCH should return updated sample");
+
   it.todo("DELETE should return deleted sample");
 });
