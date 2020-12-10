0573febfd163fc1c023287a1d7c7d12764f4c286
thlasso <thais.lasso@gmail.com>
2020-12-10 17:59:58 -0300

    refactor: extract validation functions from samples.js to modules/validate.js
    Co-authored-by: Thais Lasso <thlasso@usp.br>
    Co-authored-by: Victor Miranda <victor2142@gmail.com>

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -1,19 +1,22 @@
 export function collectorIsValid(collector) {
-    if (!collector){
-        return false;
-    }
-    
-    if (typeof collector.name !== "string" || collector.name.trim().length <= 0){
-        return false;
-    }
+  if (!collector) {
+    return false;
+  }
 
-    if (typeof collector.hospital !== "string" || collector.hospital.trim().length <= 0){
-        return false;
-    }
-    
-    return true;
+  if (typeof collector.name !== 'string' || collector.name.trim().length <= 0) {
+    return false;
+  }
+
+  if (
+    typeof collector.hospital !== 'string' ||
+    collector.hospital.trim().length <= 0
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 export function patientIdIsValid(id) {
-    return typeof id === "string" && id.trim().length > 0;
+  return typeof id === 'string' && id.trim().length > 0;
 }
