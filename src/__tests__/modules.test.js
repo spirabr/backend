cd95e15f1ae4002fb2cb9840cbd7600b285afe94
thlasso <thais.lasso@gmail.com>
2020-12-10 18:07:25 -0300

    chore: create test file for modules folder
    Co-authored-by: Thais Lasso <thlasso@usp.br>
    Co-authored-by: Victor Miranda <victor2142@gmail.com>

---
+++ src/__tests__/modules.test.js
@@ -0,0 +1,56 @@
+import { collectorIsValid, patientIdIsValid } from "../modules/validate";
+
+describe("validate.js", () => {
+    describe("collectorIsValid", () => {
+        it("should return true if collector is a valid object", () => {
+            const collector = {name: "Thais", hospital: "Albert Einstein"};
+            expect(collectorIsValid(collector)).toBe(true);
+        });
+        it("should return false if collector is not an object", () => {
+            let collector = "Albert";
+            expect(collectorIsValid(collector)).toBe(false);
+
+            collector = 1;
+            expect(collectorIsValid(collector)).toBe(false);
+
+            collector = null;
+            expect(collectorIsValid(collector)).toBe(false);
+
+            collector = undefined;
+            expect(collectorIsValid(collector)).toBe(false);
+        });
+        it("should return false if collector.name is not a string", () => {
+            const collector = {name: 1, hospital: "Albert Einstein"};
+            expect(collectorIsValid(collector)).toBe(false);
+
+            collector.name = null;
+            expect(collectorIsValid(collector)).toBe(false);
+
+            collector.name = undefined;
+            expect(collectorIsValid(collector)).toBe(false);
+        });
+        it("should return false if collector.hospital is not a string", () => {
+            const collector = {name: "Thais", hospital: 1};
+            expect(collectorIsValid(collector)).toBe(false);
+
+            collector.hospital = null;
+            expect(collectorIsValid(collector)).toBe(false);
+
+            collector.hospital = undefined;
+            expect(collectorIsValid(collector)).toBe(false);
+        });
+        it("should return false if either collector.name or collector.hospital is an empty string", () => {
+            const collector1 = {name: "", hospital: "Albert Einstein"};
+            const collector2 = {name: "Thais", hospital: ""};
+            
+            expect(collectorIsValid(collector1)).toBe(false);
+            expect(collectorIsValid(collector2)).toBe(false);
+        });
+    });
+
+    describe("patientIdIsValid", () => {
+        it.todo("should return true if patient id is valid");
+        it.todo("should return false if patient id is not a string or an empty string");
+        it.todo("should return false if patient id is a string composed by spaces");
+    });
+});
