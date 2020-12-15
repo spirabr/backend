4ba68d905990a2c6ca5f4c46cc039536965d01e7
thlasso <thais.lasso@gmail.com>
2020-12-15 16:17:37 -0300

    chore: add fallback port to app.js
    Co-authored-by: Ricardo Kojo <ricardokojo7@gmail.com>
    Co-authored-by: Thais Lasso <thlasso@usp.br>

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { setupDb } from "./db/mongoConnection";
 
 // eslint-disable-next-line import/prefer-default-export
 export const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
