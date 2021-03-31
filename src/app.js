f5d66066d1c47ea8eabc5ce44fd61e43e5857fe0
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-03-31 18:38:28 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 
 import router from "./routes/routes.js";
 import { setupDb } from "./db/mongoConnection";
+
+dotenv.config()
 
 // eslint-disable-next-line import/prefer-default-export
 export const app = express();
@@ -10,12 +14,12 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(router);
 
 const start = async () => {
   await setupDb();
-
   app.listen(port, () => console.log(`server running on port ${port}`));
 };
 
